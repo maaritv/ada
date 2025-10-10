e576b99be9b58248a2683201b139da2da269508e
Sovellus Kehittäjä <test_programmer@LUWFVFL50WT1WGC.local>
2025-10-10 14:50:18 +0300

    Creation of locationId surrogate key reference to earthquakes now works.

--- 3DataWarehousing/galaxy/earthquakes/transform/addLocationDimensionReferenceToEQ.js
+++ 3DataWarehousing/galaxy/earthquakes/transform/addLocationDimensionReferenceToEQ.js
@@ -5,18 +5,44 @@ const jsonLocations = require('../../data/location_dimension.json')
     run in ../galaxy/earthquakes/transform -directory
 */
 
-function findIdForLocation(x) {
-  const location = jsonLocations.find(jdte => jdte.location_name === x)
+
+/**
+ * Earthquake has latitude and longitude fields from 
+ * which location string must be transformed to match
+ * location_name in location dimension for getting 
+ * surrogate key reference.
+ * @param {earthquake} earthquake 
+ * @returns 
+ */
+
+function findIdForLocation(earthquake) {
+
+  function getLocationStringFromEQ(eq) {
+    console.log(eq.latitude + " " + eq.longitude)
+
+    if (eq.latitude && eq.longitude) {
+      const latitude = eq.latitude+""
+      const longitude = eq.longitude+""
+      //console.log("latitude and longitude as strings "+latitude + ", " + longitude)
+      const lat = latitude.substring(0, latitude.indexOf("."))
+      const long = longitude.split(".")[0]
+      const locationString = lat + ", " + long;
+      //console.log("location string for mapping: " + locationString)
+      return locationString;
+    }
+  }
+  const eqLocationName = getLocationStringFromEQ(earthquake)
+  const location = jsonLocations.find(jdte => jdte.location_name === eqLocationName)
   return location ? location.location_key : null   // turva nullille, jos ei löydy
 }
 
 function addLocationReference(eq) {
   console.log(eq)
-  const locationStr=eq.lat+", "+eq.long;   //Note one enmpty space !
+  const locationStr = eq.lat + ", " + eq.long;   //Note one enmpty space !
   console.log(locationStr)
   return {                           // return the eq object after locationId field is added!
     ...eq,
-    locationId: findIdForLocation(locationStr)
+    locationId: findIdForLocation(eq)
   }
 }
 
--- 3DataWarehousing/galaxy/earthquakes/transform/transform_earthquakes.js
+++ 3DataWarehousing/galaxy/earthquakes/transform/transform_earthquakes.js
@@ -35,4 +35,3 @@ function writeFile(name, jsondagta) {
 
 
 
-
