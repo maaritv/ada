a37e5b3790e1f7dfc6533facec78180560dc68bf
Sovellus Kehittäjä <test_programmer@MacBookAir.lan>
2025-11-27 21:47:31 +0200

    Added diff CDC example for SCD type 0. Add new rows based on diff.

--- 9dataingestion/diff_cdc.py
+++ 9dataingestion/diff_cdc.py
@@ -17,7 +17,10 @@ df_new = read_json_file('./bookdata/booksnew.json')
 # Find records in new_data that are not in old_data
 # use primary key, because it does not change for the
 # object.
+# SCD type 0 -> add new records do not update changed
+# records.
 df_diff = df_new[~df_new["id"].isin(df_old["id"])]
 
 print(f"Do incremental load with only {len(df_diff)} rows.")
 print(df_diff)
+
