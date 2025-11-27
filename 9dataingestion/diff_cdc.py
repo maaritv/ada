c6e50bc46ba2361389b7a091ac12b470070a3fc8
Sovellus Kehittäjä <test_programmer@MacBookAir.lan>
2025-11-27 20:08:47 +0200

    Example of diff cdc.

---
+++ 9dataingestion/diff_cdc.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+
+
+def read_json_file(file_name):
+    with open(file_name, "r") as f:
+        data_list = json.load(f)
+
+    df = pd.DataFrame(data_list)
+    return df
+
+# Convert to dataframes
+
+df_old = read_json_file('./bookdata/booksold.json')
+df_new = read_json_file('./bookdata/booksnew.json')
+
+# Find records in new_data that are not in old_data
+# use primary key, because it does not change for the
+# object.
+df_diff = df_new[~df_new["id"].isin(df_old["id"])]
+
+print(f"Do incremental load with only {len(df_diff)} rows.")
+print(df_diff)
